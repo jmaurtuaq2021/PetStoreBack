2518ea9dd00b121dd0b1a6fca6ef4c45d9a3b812
Jerico Segura <JericoSegura@gmail.com>
2022-09-07 14:58:57 -0500

    Creando el proyecto base

--- src/test/java/services/ResponseProyectoNEServices.java
+++ src/test/java/services/ResponseProyectoNEServices.java
@@ -3,7 +3,6 @@ package services;
 import com.proyecto.pacifico.config.Settings;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-import org.json.simple.JSONObject;
 
 public class ResponseProyectoNEServices {
 
--- src/test/java/steps/FuncionalidadNESteps.java
+++ src/test/java/steps/FuncionalidadNESteps.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class SolicitudNESteps {
+public class FuncionalidadNESteps {
 
     @DataTableType
     public ServicioModel convertServiceModel(Map<String, String> entry) {
